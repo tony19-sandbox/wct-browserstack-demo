ad5277f55ca04b19ced03b678111acfe47e0f844
Tony Trinh <tony19@gmail.com>
2016-06-08 20:59:02 -0500

    :zap: Add BrowserStack config
    This requires running the local BrowserStack binary before running
    WCT remote tests. It also requires two environment variables:
    
      BROWSERSTACK_USER = BrowserStack username
      BROWSERSTACK_KEY  = access key from BrowserStack profile
    
    Example run:
    
      $ BROWSERSTACK_USER=john BROWSERSTACK_KEY=ab...12 gulp wct:remote
    
    BrowserStack binary:
    https://www.browserstack.com/local-testing#command-line
    
    This will be replaced with BrowserStack plugin:
    https://www.npmjs.com/package/gulp-browserstack

--- wct.conf.js
+++ wct.conf.js
@@ -15,4 +15,31 @@ mapping['/components/' + rootPath  +
 
 ret.webserver.pathMappings.push(mapping);
 
+/**
+ * Adds BrowserStack config
+ */
+function configBrowserStack(config) {
+  var user = process.env.BROWSERSTACK_USER;
+  var key = process.env.BROWSERSTACK_KEY;
+  if (!user || !key) {
+    throw new Error('Missing BrowserStack credentials. Did you forget to set BROWSERSTACK_USER and/or BROWSERSTACK_KEY?');
+  }
+
+  var url = process.env.BROWSERSTACK_URL
+    || 'http://' + user + ':' + key + '@hub.browserstack.com/wd/hub';
+
+  var ALL_BROWSERS = require(__dirname + '/default-browserstack-browsers.json');
+  var browsers = ALL_BROWSERS.map(function(b) {
+    b['browserstack.local'] = 'true';
+    b['browserstack.debug'] = 'true';
+    b['url'] = url;
+    return b;
+  });
+
+  config.activeBrowsers = config.activeBrowsers || [];
+  config.activeBrowsers = config.activeBrowsers.concat(browsers);
+}
+
+configBrowserStack(ret);
+
 module.exports = ret;
